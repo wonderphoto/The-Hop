b38c83a4683d0d22c2ef35c17e83dd029c8b2f37
Jon Dewey <donjewey@gmail.com>
2022-04-02 10:04:27 -0700

    pull commit

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 
-class App extends Component() {
-  constructor(props) {
+class App extends Component {
+  constructor(props){
     super(props);
-
-    state = {};
   }
+  
   render() {
     return <div>What's Hoppening?</div>;
   }
