1b0eafbb1a49e44c94500f293de21b1408bcbab8
Robert Bedenbaugh <robertbedenbaughjr@gmail.com>
2022-04-02 12:31:05 -0400

    20220402 12:30

--- src/client/App.js
+++ src/client/App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from "react";
 
 class App extends Component() {
@@ -11,28 +12,4 @@ class App extends Component() {
   }
 }
 
-                            <div className="text-center lg:text-left">
-                                <button
-                                    type="button"
-                                    className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-md leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                                    onClick={() => { }}
-                                >
-                                    Login
-                                </button>
-                                <p className="text-md font-semibold mt-2 pt-1 mb-0">
-                                    Don't have an account?
-                                    <button
-                                        onClick={() => {}}
-                                        className="ml-2 text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
-                                    > Register
-                                    </button>
-                                </p>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
-    )
-}
+export default App;
