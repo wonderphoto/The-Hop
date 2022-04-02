b972de199aaa701c5da784ccc6df7461d11d69af
Gerry Bong <ggbong734@gmail.com>
2022-04-02 11:24:51 -0700

    added login and signup pages

--- src/client/App.js
+++ src/client/App.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from './components/HomePage';
+import { LoginPage } from './components/LoginPage';
+import { SignupPage } from './components/SignupPage';
+import { ProfilePage } from './components/ProfilePage';
+
 
 function App() {
 
   return (
-    <HomePage />
-
-    // <BrowserRouter>
-    // <Routes>
-    //     <Route path="/" element={<HomePage />}></Route>
-    // <Route path="/login" element={<LoginPage />}></Route>
-    //     <Route path="/signup" element={<SignupPage />}></Route>
-    //     <Route path="/profile" element={<ProfilePage />}></Route>
-    // </Routes>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path="/profile" element={<ProfilePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
