390fdd0591b3b08016630af11bb6c78de7590a41
Gerry Bong <ggbong734@gmail.com>
2022-04-03 23:09:01 -0700

    started map, changedlibrary to @react-google-maps, changed style, changed onLoad

--- src/client/components/homepage/Map.js
+++ src/client/components/homepage/Map.js
@@ -1,27 +1,49 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, { Component, useMemo, useCallback, useRef, useState } from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
+
+const containerStyle = {
+    width: '100%',
+    height: '85vh'
+};
 
 export const Map = () => {
-    const coordinates = { lat: 37.382483, lng: -122.209946 };
+    const [base, setBase] = useState(0);
 
-    return (
-        //
-        <div className="border-4 border-blue-400 h-auto w-full">
-            <div className="bg-gray-200" style={{ height: '80vh', width: '100%' }}>
-                {/* <GoogleMapReact
-                    bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS }}
-                    defaultCenter={coordinates}
-                    center={coordinates}
-                    defaultZoom={14}
-                    margin={[50, 50, 50, 50]}
-                    options={''}
-                    onChange={() => { }}
-                    onChildClick={() => { }}
-                >
-                </GoogleMapReact> */}
-            </div>
-            <div className='text-xl text-blue-500 font-bold'>map</div>
-        </div>
-    );
+    const center = useMemo(() => ({ lat: 37.768, lng: -122.42 }), []);
+    const options = useMemo(() => ({
+        mapId: "4db6fc355c1b4a66",
+        disableDefaultUI: true,
+        clickableIcons: false,
+    }), []);
+
+    const mapRef = useRef();
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.GOOGLE_MAPS
+    })
+
+    const [map, setMap] = React.useState(null)
+
+    const onLoad = useCallback((map) => (mapRef.current = map), []);
+
+    const onUnmount = useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            defaultCenter={center}
+            center={center}
+            zoom={14}
+            options={options}
+        // onLoad={onLoad}
+        // onUnmount={onUnmount}
+        >
+            { /* Child components, such as markers, info windows, etc. */}
+            <></>
+        </GoogleMap>
+    ) : <div>Loading ....</div>
 }
