305bda8796a57b9265fa647fa655a59e3ec6933a
Robert Bedenbaugh <robertbedenbaughjr@gmail.com>
2022-04-04 20:35:58 -0400

    Profile page updates

--- src/client/components/ProfilePage.js
+++ src/client/components/ProfilePage.js
@@ -3,20 +3,23 @@ import { Header } from './Header'
 import { ProfileCard } from './profilepage/ProfileCard'
 import { SavedEventsCard } from './profilepage/SavedEventsCard'
 import { ScheduleCard } from './profilepage/ScheduleCard'
+import { Footer } from './Footer'
 
 
 export const ProfilePage = () => {
   return (
-    <div className='flex-col'>
+    <div className='flex flex-col h-screen'>
       <Header />
-        <div className="flex-direction: row">
-          <ProfileCard />
-          <ScheduleCard />
-          <SavedEventsCard />
+        <div className="">
+          <div className="inline-flex flex-col justify-items-start h-screen w-1/2">
+            <ProfileCard />
+          < SavedEventsCard />
+          </div>
+          <div className="inline-flex flex-col justify-end h-screen justify-end w-1/2"> 
+            <ScheduleCard />
+          </div>
         </div>
       <Footer />
     </div>
   )
 }
-
-
