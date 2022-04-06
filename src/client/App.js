a20c46fef044516d1015c2e37cc491d523638df6
Jon Dewey <donjewey@gmail.com>
2022-04-05 19:46:33 -0700

    session frontend updates

--- src/client/App.js
+++ src/client/App.js
@@ -1,24 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from './components/HomePage';
-import { LoginPage } from './components/LoginPage';
-import { SignupPage } from './components/SignupPage';
-import { ProfilePage } from './components/ProfilePage';
-
+import { HomePage } from "./components/HomePage";
+import { LoginPage } from "./components/LoginPage";
+import { SignupPage } from "./components/SignupPage";
+import { ProfilePage } from "./components/ProfilePage";
 
 function App() {
   const [user, setUser] = useState({});
 
+  useEffect(() => {
+    const sessionCheck = async () => {
+      const response = await fetch("http://localhost:3000/auth", {
+        credentials: "include",
+      }).then((res) => res.json());
+      console.log(response)
+      if (response.isLoggedIn) {
+        setUser(response);
+      }
+    };
+    sessionCheck();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage user={user} setUser={setUser} />}></Route>
+        <Route
+          path="/"
+          element={<HomePage user={user} setUser={setUser} />}
+        ></Route>
         <Route path="/login" element={<LoginPage setUser={setUser} />}></Route>
-        <Route path="/signup" element={<SignupPage setUser={setUser} />}></Route>
+        <Route
+          path="/signup"
+          element={<SignupPage setUser={setUser} />}
+        ></Route>
         <Route path="/profile" element={<ProfilePage />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Header = ({ user, setUser }) => {
   let navigate = useNavigate();
 
   const navLogin = () => {
     navigate("/login");
-  }
+  };
+
+  const navProfile = () => {
+    navigate("/profile");
+  };
 
   const logout = () => {
-    fetch('http://localhost:3000/auth/logout', {
-      method: 'POST',
+    fetch("http://localhost:3000/auth/logout", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ "username": user.username, "userid": user.userid }),
-    }).then(response => response.json())
-      .then(user => {
-        console.log("user has been successfully logged out and session destroyed")
+      body: JSON.stringify({ username: user.username, userid: user.userid }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        console.log(
+          "user has been successfully logged out and session destroyed"
+        );
         // reset user state to empty object
         setUser({});
         window.location.reload(true);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+  };
 
   //   return (
   //     <div className='w-full py-4 border-color-4 border-green-400 flex justify-between'>
@@ -45,65 +51,95 @@ export const Header = ({ user, setUser }) => {
   return (
     <nav className="bg-green-300 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com" className="flex items-center">
-          {/* <img src="/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="" /> */}
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">The Hop!</span>
+        <a className="flex items-center">
+          <img
+            src="../../../../public/img/logo.png"
+            className="mr-3 h-6 sm:h-9"
+            alt=""
+          />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white" onClick={navProfile}>
+            The Hop!
+          </span>
         </a>
-        <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-            </li>
-          </ul>
-        </div>
+
         {JSON.stringify(user) !== JSON.stringify({}) ? (
-          <div className="flex items-center md:order-2">
-            <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="dropdown">
-              <span className="sr-only">Open user menu</span>
-              <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="" />
+          <div>
+            <button
+              id="dropdownDefault"
+              data-dropdown-toggle="dropdownProfile"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              type="button"
+              // onClick={document.getElementById('dropdownProfile').style.display = 'flex'}
+            >
+              Dropdown button{" "}
+              <svg
+                className="ml-2 w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
+              </svg>
             </button>
-            {/* <!-- Dropdown menu --> */}
-            <div className="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown">
-              <div className="py-3 px-4">
-                <span className="block text-sm text-gray-900 dark:text-white">{user.username}</span>
-                <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">{user.email}</span>
-              </div>
-              <ul className="py-1" aria-labelledby="dropdown">
+            <div
+              id="dropdownProfile"
+              className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
+            >
+              <ul
+                className="py-1 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownDefault"
+              >
                 <li>
-                  <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                  <a
+                    href="#"
+                    className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Dashboard
+                  </a>
                 </li>
                 <li>
-                  <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                  <a
+                    href="#"
+                    className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Settings
+                  </a>
                 </li>
                 <li>
-                  <a className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={logout}>Sign out</a>
+                  <a
+                    href="#"
+                    className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Earnings
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Sign out
+                  </a>
                 </li>
               </ul>
             </div>
-
-            <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-              <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-            </button>
           </div>
-        ) :
-          (<div className="flex items-center md:order-2"><button type="button" className="inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out" onClick={navLogin}>Login</button></div>)}
+        ) : (
+          <button
+            type="button"
+            className="inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out"
+            onClick={navLogin}
+          >
+            Login
+          </button>
+        )}
       </div>
-      {/* {JSON.stringify(user) === JSON.stringify({}) ?
-        <button onClick={() => navigate('/login')}>Login</button>
-        :
-        <button onClick={() => logout()}>Logout</button>
-      } */}
     </nav>
-  )
-}
+  );
+};
