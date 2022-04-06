7361f47df572dab049f3f1a3e6c423ed15b822a3
lauren-acrich <94992940+lauren-acrich@users.noreply.github.com>
2022-04-05 19:37:54 -0700

    rendered saved event information

--- src/client/components/ProfilePage.js
+++ src/client/components/ProfilePage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Header } from './Header'
-import { ProfileCard } from './profilepage/ProfileCard'
 import { SavedEventsContainer } from './profilepage/SavedEventsContainer'
 import { ScheduleCard } from './profilepage/ScheduleCard'
 import { Footer } from './Footer'
@@ -12,7 +11,6 @@ export const ProfilePage = ({ user, setUser }) => {
       <Header user={ user } setUser={ setUser } />
         <div className="font-serif">
           <div className="inline-flex flex-col justify-items-start h-screen w-1/2">
-            <ProfileCard />
             <SavedEventsContainer user={user} />
           </div>
           <div className="inline-flex flex-col justify-end h-screen justify-end w-1/2"> 
