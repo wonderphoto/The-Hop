385ea20e321d737db17cc5ebaa38eca73c236df1
Gerry Bong <ggbong734@gmail.com>
2022-04-07 23:10:51 -0700

    fixed merge conflict with origin

--- src/client/components/homepage/Sidebar.js
+++ src/client/components/homepage/Sidebar.js
@@ -5,13 +5,7 @@ import { EventsContainer } from './EventsContainer'
 export const Sidebar = ({ apiEvents, setApiEvents, user, setMapBase, mapRef, setCircleRadius }) => {
 
   return (
-<<<<<<< HEAD
     <div className='flex-col h-auto w-1/3'>
-||||||| constructed merge base
-    <div className='flex-col h-auto w-1/3 border-2 border-red-300'>
-=======
-    <div className='flex-col h-auto w-1/3 border-r-4 border-gray-300'>
->>>>>>> increased limit per api call to 30
 
       <SearchBox apiEvents={apiEvents} setApiEvents={setApiEvents} setMapBase={setMapBase} mapRef={mapRef} setCircleRadius={setCircleRadius} />
       <EventsContainer apiEvents={apiEvents} user={user} />
