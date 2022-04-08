6ba0b8d2472f9fe8bcb5ba0786c8e21cc93b2782
lauren-acrich <94992940+lauren-acrich@users.noreply.github.com>
2022-04-08 14:46:24 -0700

    added animation

--- src/client/components/homepage/EventsContainer.js
+++ src/client/components/homepage/EventsContainer.js
@@ -14,18 +14,6 @@ export const EventsContainer = ({ apiEvents, user }) => {
                     <p className='font-medium text-md text-gray-600'>Please enter a query above to see events</p>
                 </div>
             }
-
-            {apiEvents.length === 0 ?
-                <div className="h-auto bg-white">
-                    <a>
-                        <img
-                        src="https://media0.giphy.com/media/fUSQGDRvuBlQXcX0TA/200.webp?cid=ecf05e47ry7vnhwjqyngbdp5snx8gwdbxxm6pchbd9xwsvpe&rid=200.webp&ct=g"
-                        id="bunny"
-                        alt="" />
-                    </a>
-                </div>
-                : <div></div>
-            }
             
             {apiEvents.length > 0 ?
                 <div className='p-4 h-[33rem] space-y-2 overflow-y-scroll relative'>
@@ -35,8 +23,13 @@ export const EventsContainer = ({ apiEvents, user }) => {
                     }
                 </div>
                 :
-                <div className='flex relative h-[33rem] bg-slate-50'>
-
+                <div className='flex relative h-[33rem] bg-white'>
+                <a>
+                        <img
+                        src="https://media0.giphy.com/media/fUSQGDRvuBlQXcX0TA/200.webp?cid=ecf05e47ry7vnhwjqyngbdp5snx8gwdbxxm6pchbd9xwsvpe&rid=200.webp&ct=g"
+                        id="bunny"
+                        alt="" />
+                    </a>
                 </div>
             }
 
